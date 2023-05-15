22f9101e9aac34ae85bfd179186d4adc4acebb83
unknown <anour2168@gmail.com>
2023-05-15 12:11:42 +0200

    make search functionality

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 // import { Quotes } from "../../Models/interfaces/quotes";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchAllQuotes } from "../../Middlewares/FetchAllQuotes/FetchAllQuotes";
 import { useAppDispatch, useAppSelector } from "../../Store/hooks";
 import { QouteState } from "../../Slices/Quotes";
 import Card from "../../Components/Card/Card";
+import { Quotes } from "../../Models/interfaces/quotes";
 
 const Home = () => {
+  const [query, setQuery] = useState("");
   const dispatch = useAppDispatch();
   const { QuotesList, loading } = useAppSelector(QouteState);
   useEffect(() => {
@@ -14,12 +16,29 @@ const Home = () => {
   if (loading) {
     return <div>Loading......</div>;
   }
+  // Search Function (search by author or content)
+  const search = (QuotesList: Quotes[]) => {
+    return QuotesList.filter(
+      (item) =>
+        item.author.toLocaleLowerCase().includes(query) ||
+        item.content.toLocaleLowerCase().includes(query)
+    );
+  };
   return (
     <>
-      <h1 className="font-bold text-red-500 text-center uppercase">Home</h1>
-
+      <h1 className="font-bold text-red-500 text-center text-2xl font-pacifico uppercase">
+        --- Quotes Page ---
+      </h1>
+      <div className="mt-10">
+        <input
+          type="search"
+          placeholder="Search........."
+          className="outline-none border-2 border-red-500 rounded-3xl px-5 md:py-3 py-2 md:w-96  w-full"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </div>
       <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-5">
-        <Card QuotesList={QuotesList} />
+        <Card QuotesList={search(QuotesList)} />
       </div>
     </>
   );
